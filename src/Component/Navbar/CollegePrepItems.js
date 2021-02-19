de443e37539684534671a0b17685a071ff87a6b0
mira <n.tamikan@gmail.com>
2021-02-19 16:37:05 +0800

    Navbar (first version) - zereg unadag dropdown xD

---
+++ src/Component/Navbar/CollegePrepItems.js
@@ -0,0 +1,38 @@
+export const CollegePrepItems = [
+    {
+        id: "0",
+        name: "International Exams",
+        cName: 'list-item',
+        path: '/international-exams'
+    },
+    {
+        id: "1",
+        name: "Scholarships",
+        cName: 'list-item',
+        path: '/scholarships'
+    },
+    {
+        id: "2",
+        name: "Top Colleges",
+        cName: 'list-item',
+        path: '/top-colleges'
+    },
+    {
+        id: "3",
+        name: "Choose Your Major",
+        cName: 'list-item',
+        path: '/choose-your-major'
+    },
+    {
+        id: "4",
+        name: "Ideal Plan",
+        cName: 'list-item',
+        path: 'ideal-plan'
+    },
+    {
+        id: "5",
+        name: "Extracurriculars",
+        cName: 'list-item',
+        path: 'extracurriculars'
+    }
+]
